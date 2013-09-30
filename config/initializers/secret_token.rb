ae8b55909f0c17092b57c09861bacb8997298fac
Justin <justin@roomsurf.com>
2013-09-30 19:02:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YOURAppNameHere::Application.config.secret_key_base = '9841d21a32b919677886453882566e5d74ab0c2c2a51b82b00ae5946fa3867bd8b2e7e8d3a544c678d187a90b9b16ab6cc90784931773d1ba17d760946de7d3f'
